611a94df8869165bfff564428e59c7b26927e526
ChrisWilson <divclass01@gmail.com>
2020-10-28 14:15:37 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// zomato_key = da1eaf3933827d15b2d77c493c2d0cb6
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
